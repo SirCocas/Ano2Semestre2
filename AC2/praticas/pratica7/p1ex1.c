f51b68338f6382b762177856cbf3963e67c61721
SirCocas <sofiateixeiravaz@sapo.pt>
2020-04-01 10:18:59 +0100

    check this

---
+++ AC2/praticas/pratica7/p1ex1.c
@@ -0,0 +1,26 @@
+#include <detpic32.h>
+#include "../utils.c"
+
+
+int main(void){
+    int counter = 0;
+
+    // configure timer t3 (2 Hz with interrupts disabled)
+    T3CONbits.TCKPS = 5; //1:32 prescaler
+    PR3= 2;  //fout = 20MHz / (32*(2+1)) = 2 Hz
+    TMR3 = 0; //reset timer 3
+    T3CONbits.TON= 1 ; //enable timer 3
+    while(1){
+        //wait for T3IF = 1
+        while(IFS0bits.T3IF == 0);
+
+        //reset T3IF
+        IFS0bits.T3IF == 0;
+
+        counter++;
+        printInt10(counter);
+        if(counter == 120)
+            counter = 0;
+    }
+    return 0;
+}
