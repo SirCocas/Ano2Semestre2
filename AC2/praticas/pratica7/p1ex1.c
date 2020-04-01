77661951e807f26af510c2f5e38a44e66914c859
SirCocas <sofiateixeiravaz@sapo.pt>
2020-04-01 10:41:18 +0100

    asd

--- AC2/praticas/pratica7/p1ex1.c
+++ AC2/praticas/pratica7/p1ex1.c
@@ -6,8 +6,8 @@ int main(void){
     int counter = 0;
 
     // configure timer t3 (2 Hz with interrupts disabled)
-    T3CONbits.TCKPS = 5; //1:32 prescaler
-    PR3= 312499;  //fout = 20MHz / (32*(312499+1)) = 2 Hz
+    T3CONbits.TCKPS = 7; 		// 1:256 prescaler
+	PR3 = 39062;				// Fout = 2 Hz
     TMR3 = 0; //reset timer 3
     T3CONbits.TON= 1 ; //enable timer 3
     while(1){
